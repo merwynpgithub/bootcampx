a989e1db550484277ff6e93c7eca6a777289a638
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2022-01-21 14:11:41 -0500

    add data about each assistance request in 4_queries

--- 4_queries/4_queries.sql
+++ 4_queries/4_queries.sql
@@ -9,3 +9,10 @@ FROM assistance_requests
 JOIN students ON students.id = student_id
 WHERE students.name = 'Elliot Dickinson'
 GROUP BY students.name;
+
+SELECT teachers.name as teacher, students.name as student, assignments.name as assignment, (completed_at-started_at) as duration
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN assignments ON assignments.id = assignment_id
+ORDER BY duration;
